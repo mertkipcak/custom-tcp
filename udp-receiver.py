27860235dc62d8508fe89b2c2fe2130cdb0d3107
mkipcak <mkipcak@demonware.net>
2023-04-02 14:41:49 -0700

    Updated the ip addresses

--- udp-receiver.py
+++ udp-receiver.py
@@ -4,7 +4,7 @@ def main():
     # Create a UDP socket
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-    server_address = ('localhost', 10000)
+    server_address = ('192.168.1.100', 10000)
     sock.bind(server_address)
 
     while True:
@@ -12,6 +12,9 @@ def main():
         data, address = sock.recvfrom(4096)
 
         print(f'Received message: {data} from {address}')
+
+        # Echo the message back to the sender
+        sent = sock.sendto(data, address)
 
 if __name__ == '__main__':
     main()
--- udp_sender.py
+++ udp_sender.py
@@ -4,13 +4,18 @@ def main():
     # Create a UDP socket
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-    server_address = ('localhost', 10000)
+    server_address = ('192.168.1.100', 10000)
     message = b'This is a test message.'
 
     try:
         # Send the message to the server
         print(f'Sending message: {message}')
         sent = sock.sendto(message, server_address)
+
+        # Receive the echoed message
+        print('Waiting for the echoed message')
+        data, address = sock.recvfrom(4096)
+        print(f'Received echoed message: {data} from {address}')
 
     finally:
         print('Closing socket')
